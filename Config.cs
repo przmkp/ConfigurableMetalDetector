6376d29fa92d7cfa3958a9040e9ea8b9995809a0
przmkp <przemyslawpak@gmail.com>
2021-06-30 21:05:24 +0200

    Dodaj pliki projektów.

---
+++ Config.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+using Terraria.ModLoader.Config.UI;
+using Terraria.UI;
+
+namespace ConfigurableMetalDetector
+{
+    public class Config : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        [Label("Pot")]
+        [DefaultValue(100)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int PotValue;
+
+        [Label("Copper Ore")]
+        [DefaultValue(200)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int CopperValue;
+
+        [Label("Tin Ore")]
+        [DefaultValue(210)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int TinValue;
+
+        [Label("Iron Ore")]
+        [DefaultValue(220)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int IronValue;
+
+        [Label("Lead Ore")]
+        [DefaultValue(230)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int LeadValue;
+
+        [Label("Silver Ore")]
+        [DefaultValue(240)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int SilverValue;
+
+        [Label("Tungsten Ore")]
+        [DefaultValue(250)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int TungstenValue;
+
+        [Label("Meteorite")]
+        [DefaultValue(300)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int MeteoriteValue;
+
+        [Label("Demonite Ore")]
+        [DefaultValue(310)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int DemoniteValue;
+
+        [Label("Crimtane Ore")]
+        [DefaultValue(320)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int CrimtaneValue;
+
+        [Label("Sturdy Fossil")]
+        [DefaultValue(350)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int FossilValue;
+
+        [Label("Gold Ore")]
+        [DefaultValue(400)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int GoldValue;
+
+        [Label("Platinum Ore")]
+        [DefaultValue(410)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int PlatinumValue;
+
+        [Label("Chest")]
+        [DefaultValue(500)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int ChestValue;
+
+        [Label("Cobalt Ore")]
+        [DefaultValue(600)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int CobaltValue;
+
+        [Label("Palladium Ore")]
+        [DefaultValue(610)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int PalladiumValue;
+
+        [Label("Mythril Ore")]
+        [DefaultValue(620)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int MythrilValue;
+
+        [Label("Orichalcum Ore")]
+        [DefaultValue(630)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int OrichalcumValue;
+
+        [Label("Adamantite Ore")]
+        [DefaultValue(640)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int AdamantiteValue;
+
+        [Label("Titanium Ore")]
+        [DefaultValue(650)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int TitaniumValue;
+
+        [Label("Chlorophyte Ore")]
+        [DefaultValue(700)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int ChlorophyteValue;
+
+        [Label("Life Crystal")]
+        [DefaultValue(510)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int LifeCrystalValue;
+
+        [Label("Life Fruit")]
+        [DefaultValue(810)]
+        [Range(0, 5000)]
+        [Increment(10)]
+        public int LifeFruitValue;
+    }
+}
---
+++ ConfigurableMetalDetector.cs
@@ -0,0 +1,39 @@
+using Terraria.ModLoader;
+using Terraria;
+
+
+namespace ConfigurableMetalDetector
+{
+	public class ConfigurableMetalDetector : Mod
+	{
+        public override void Load()
+        {
+			Main.tileValue[28] = (short)ModContent.GetInstance<Config>().PotValue;
+			Main.tileValue[7] = (short)ModContent.GetInstance<Config>().CopperValue;
+			Main.tileValue[166] = (short)ModContent.GetInstance<Config>().TinValue;
+			Main.tileValue[6] = (short)ModContent.GetInstance<Config>().IronValue;
+			Main.tileValue[167] = (short)ModContent.GetInstance<Config>().LeadValue;
+			Main.tileValue[9] = (short)ModContent.GetInstance<Config>().SilverValue;
+			Main.tileValue[168] = (short)ModContent.GetInstance<Config>().TungstenValue;
+			Main.tileValue[37] = (short)ModContent.GetInstance<Config>().MeteoriteValue;
+			Main.tileValue[22] = (short)ModContent.GetInstance<Config>().DemoniteValue;
+			Main.tileValue[204] = (short)ModContent.GetInstance<Config>().CrimtaneValue;
+			Main.tileValue[407] = (short)ModContent.GetInstance<Config>().FossilValue;
+			Main.tileValue[8] = (short)ModContent.GetInstance<Config>().GoldValue;
+			Main.tileValue[169] = (short)ModContent.GetInstance<Config>().PlatinumValue;
+			Main.tileValue[21] = (short)ModContent.GetInstance<Config>().ChestValue;
+			Main.tileValue[467] = (short)ModContent.GetInstance<Config>().ChestValue;
+			Main.tileValue[441] = (short)ModContent.GetInstance<Config>().ChestValue;
+			Main.tileValue[468] = (short)ModContent.GetInstance<Config>().ChestValue;
+			Main.tileValue[107] = (short)ModContent.GetInstance<Config>().CobaltValue;
+			Main.tileValue[221] = (short)ModContent.GetInstance<Config>().PalladiumValue;
+			Main.tileValue[108] = (short)ModContent.GetInstance<Config>().MythrilValue;
+			Main.tileValue[222] = (short)ModContent.GetInstance<Config>().OrichalcumValue;
+			Main.tileValue[111] = (short)ModContent.GetInstance<Config>().AdamantiteValue;
+			Main.tileValue[223] = (short)ModContent.GetInstance<Config>().TitaniumValue;
+			Main.tileValue[211] = (short)ModContent.GetInstance<Config>().ChlorophyteValue;
+			Main.tileValue[12] = (short)ModContent.GetInstance<Config>().LifeCrystalValue;
+			Main.tileValue[236] = (short)ModContent.GetInstance<Config>().LifeFruitValue;
+		}
+    }
+}
